0732e5800147942d3c725c712fecd5b31be6e7ae
reyhanehkarimi <reyhanehkarimi9978@gmail.com>
2024-12-12 11:51:37 +0330

    final-edit

--- 0-Explanation/explain.js
+++ 0-Explanation/explain.js
@@ -1,1 +1,6 @@
 // explain the difference between getBy, findBy, queryBy:
+
+// ! Method	Behavior	Use When
+// !getBy	Synchronous, throws error if no match	Element must exist immediately.
+// !findBy	Asynchronous, waits for element to appear, throws error on timeout	Element appears after a delay.
+// !queryBy	Synchronous, returns null if no match	Verify that an element does not exist.
